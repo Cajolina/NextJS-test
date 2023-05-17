22a96c11f6110a84eea58339b9581a1e2bc84294
Carro <carolinaisabellaandersson@gmail.com>
2023-05-17 22:10:38 +0200

    navigation

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>
+        <Link href="/">Home</Link>
+      </div>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default Home;
